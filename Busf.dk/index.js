2543e4142cc458e30edf39ba67ef10ec81e66378
MichelleHoiberg <michelle.hoiberg@gmail.com>
2020-03-04 12:14:14 +0100

    jaja

--- Busf.dk/index.js
+++ Busf.dk/index.js
@@ -6,26 +6,3 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
-}
-var image1=new Image()
-image1.src="img_nature_wide1.jpg"
-var image2=new Image()
-image2.src="img_snow_wide2.jpg"
-var image3=new Image()
-image3.src="img_mountains_wide3.jpg"
-
-//variable that will increment through the images
-var step=1
-function slideit(){
-//if browser does not support the image object, exit.
-if (!document.images)
-return
-document.images.slide.src=eval("image"+step+".src")
-if (step<3)
-step++
-else
-step=1
-//call function "slideit()" every 2.5 seconds
-setTimeout("slideit()",2500)
-}
-slideit()
