494570dc0ac70103aab6de56d4d68dcafdaa1d37
MichelleHoiberg <michelle.hoiberg@gmail.com>
2020-03-04 13:32:45 +0100

    waaah

--- Busf.dk/Html/Js/index.js
+++ Busf.dk/Html/Js/index.js
@@ -6,31 +6,3 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
-  var i = 0; 			// Start Point
-  var images = [];	// Images Array
-  var time = 3000;	// Time Between Switch
-
-  // Image List
-  images[0] = "https://www.w3schools.com/w3css/img_nature_wide.jpg";
-  images[1] = "https://www.w3schools.com/w3css/img_snow_wide.jpg";
-  images[2] = "https://www.w3schools.com/w3css/img_mountains_wide.jpg";
-
-  // Change Image
-  function changeImg(){
-  	document.slide.src = images[i];
-
-  	// Check If Index Is Under Max
-  	if(i < images.length - 1){
-  	  // Add 1 to Index
-  	  i++;
-  	} else {
-  		// Reset Back To O
-  		i = 0;
-  	}
-
-  	// Run function every x seconds
-  	setTimeout("changeImg()", time);
-  }
-
-  // Run function when page loads
-  window.onload=changeImg;
